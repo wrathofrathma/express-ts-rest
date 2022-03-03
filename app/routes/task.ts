e63b4985828eef9262306b757ef9eb607c0572f8
Rathma <rathma@kyso.dev>
2022-03-03 12:54:16 -0500

    Tasks create & Delete stuff without unit tests

--- app/routes/task.ts
+++ app/routes/task.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
 // Import route controller(s)
-import { ProjectController } from '../http/controllers/ProjectController';
 import { TaskController } from '../http/controllers/TaskController';
 import AuthMiddleware from '../http/middleware/AuthMiddleware';
 
@@ -11,14 +10,8 @@ const router = express.Router();
 // All of our requests should be authenticated.
 router.use(AuthMiddleware);
 
-router.get('/', ProjectController.index);
-router.post('/', ProjectController.create);
-router.patch('/:id', ProjectController.update);
-router.delete('/:id', ProjectController.delete);
-
-// Task related routes
-router.get('/:id/tasks', TaskController.index);
-router.post('/:id/tasks', TaskController.create);
+// router.patch('/:id', TaskController.update);
+// router.delete('/:id', TaskController.delete);
 
 // Export the router
 export default router;
